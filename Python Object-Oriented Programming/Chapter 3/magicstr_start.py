ae80e770efb87de340167f753a4cb7c39d00605c
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-07 19:26:41 -0700

    string representation using str and repr

--- Python Object-Oriented Programming/Chapter 3/magicstr_start.py
+++ Python Object-Oriented Programming/Chapter 3/magicstr_start.py
@@ -1,4 +1,3 @@
-# Python Object Oriented Programming by Joe Marini course example
 # Using the __str__ and __repr__ magic methods
 
 
@@ -10,8 +9,17 @@ class Book:
         self.price = price
 
     # TODO: use the __str__ method to return a string
+    # __str__ is used to provide a user friendly string description of the object
+    # and it is usually intended to be displayed to the user
+    def __str__(self):
+        return f'{self.title} by {self.author}, costs {self.price}'
 
     # TODO: use the __repr__ method to return an obj representation
+    # __repr__ is used to generate a more developer facing string
+    # ideally used to recreate the object in its current state
+    # also used LOADS for DEBUGGING purposes so its used to display loads of information
+    def __repr__(self):
+        return f'title={self.title},author={self.author},price={self.price}'
 
 
 b1 = Book("War and Peace", "Leo Tolstoy", 39.95)
@@ -19,3 +27,6 @@ b2 = Book("The Catcher in the Rye", "JD Salinger", 29.95)
 
 print(b1)
 print(b2)
+
+print(str(b1))
+print(repr(b2))
