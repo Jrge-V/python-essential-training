f5e5e319038ef6657593e43b7fe32a389cf0e069
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-13 00:28:46 -0700

    challenge 4 progress

--- Level Up Python/04 Find All List Items/find_all_list_items.py
+++ Level Up Python/04 Find All List Items/find_all_list_items.py
@@ -1,5 +1,27 @@
-def index_all():
+def index_all(example, int):
+
+    # TODO: Initialize empty list
+
+    # TODO: loop through index and value using enumerate on the list
+
+        # TODO: create an inner if statement to check if the integer is equal to value and append it,
+
+        # TODO: else check if index is a list
+
+            # TODO: recursive call the method to search for a sublist <- the hard part for me
+
+                # TODO: append index to my  list
+
+    # TODO: Return the list
+
     pass
 
 
+
+
+
 # TEST CODE
+example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
+
+# example = [[1, 2, [2]]]  # [[0, 1], [1, 0]]
+index_all(example, 2)  # [[0, 0, 1], [0, 1], [1, 1]]
