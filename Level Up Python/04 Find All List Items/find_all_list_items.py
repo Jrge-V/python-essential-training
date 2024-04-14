714e5b9699341fd6beb12a82e2dbcc1b1868870b
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-13 23:47:29 -0700

    find all list items complete

--- Level Up Python/04 Find All List Items/find_all_list_items.py
+++ Level Up Python/04 Find All List Items/find_all_list_items.py
@@ -1,27 +1,19 @@
-def index_all(example, int):
 
-    # TODO: Initialize empty list
+def index_all(my_lst, item):
+    indices = []
 
-    # TODO: loop through index and value using enumerate on the list
+    for i in range(len(my_lst)):
+        if my_lst[i] == item:
+            indices.append([i])
+        elif isinstance(my_lst[i], list):
+            for index in index_all(my_lst[i], item):
+                indices.append([i] + index)
 
-        # TODO: create an inner if statement to check if the integer is equal to value and append it,
-
-        # TODO: else check if index is a list
-
-            # TODO: recursive call the method to search for a sublist <- the hard part for me
-
-                # TODO: append index to my  list
-
-    # TODO: Return the list
-
-    pass
-
-
-
+    return indices
 
 
 # TEST CODE
-example = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
+example_list = [[[1, 2, 3], 2, [1, 3]], [1, 2, 3]]
 
 # example = [[1, 2, [2]]]  # [[0, 1], [1, 0]]
-index_all(example, 2)  # [[0, 0, 1], [0, 1], [1, 1]]
+print(index_all(example_list, 2))  # [[0, 0, 1], [0, 1], [1, 1]]
