e472dd65ff0e993493363b8f07baccdb4798929b
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-15 21:17:19 -0700

    finished challenge 7, implemented args

--- Level Up Python/07 Schedule a Function/schedule_a_function.py
+++ Level Up Python/07 Schedule a Function/schedule_a_function.py
@@ -1,15 +1,15 @@
 import time
 
 
-def schedule_function(result, p, text):
+def schedule_function(result, p, *args):
     print(f'Current time: {time.strftime("%a %b %d %H:%M:%S %Y", time.localtime())}')
     print(f'Scheduled for: {time.strftime("%a %b %d %H:%M:%S %Y", time.localtime(result))}')
     time.sleep(result - time.time())
-    p(text)
+    p(*args)
 
 
 # TEST CODE
-schedule_function(time.time() + 10, print, 'Howdy!')
+schedule_function(time.time() + 1, print, 'Howdy!', 'BEEP!')
 # print() scheduled for Sun Aug 14 20:39:28 2022
 # Then one second later...
 #
