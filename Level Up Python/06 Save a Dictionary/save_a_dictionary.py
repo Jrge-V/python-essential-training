9742ea01421bbc9adf9193502841b611f56f9a5f
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-15 17:42:49 -0700

    finishing chall 6

--- Level Up Python/06 Save a Dictionary/save_a_dictionary.py
+++ Level Up Python/06 Save a Dictionary/save_a_dictionary.py
@@ -1,11 +1,19 @@
 # Save dictionary function upon exiting program
-def save_dict():
-    pass
+def save_dict(my_dict, output_file):
+
+    with open(output_file, 'w') as file:
+        for key, value in my_dict:
+            file.write(f'{key}: {value}')
 
 
 # Load dictionary function upon execution
-def load_dict():
-    pass
+def load_dict(input_file):
+    with open(input_file, 'r') as file:
+        return file.read()
 
 
 
+
+# TEST CODE
+save_dict({1: 'a', 2: 'b', 10: 'c'}, 'test.pickle')
+print(load_dict('test.pickle'))  # {1: 'a', 2: 'b', 3: 'c'}
