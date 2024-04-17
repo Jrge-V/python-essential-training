fde4d0971b8bf48f5c9b138d275e08c89d62e6c9
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-17 16:03:20 -0700

    continuing chall 09

--- Level Up Python/09 Simulate Dice/simulate_dice.py
+++ Level Up Python/09 Simulate Dice/simulate_dice.py
@@ -1,5 +1,13 @@
-def roll_dice(*args):
-    pass
 
 
-roll_dice('hello', 'wassup')
+def roll_dice(dice_list):
+
+    min_num = len(dice_list)
+    max_num = sum(dice_list)
+    print(min_num, max_num)
+
+    print(dice_list)
+
+
+dice_list = [4, 6, 6]
+roll_dice(dice_list)
