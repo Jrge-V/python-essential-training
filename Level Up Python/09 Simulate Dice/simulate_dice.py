b7b06ed71d1c8306c7dc901bc36350e0ebb3183a
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-18 21:16:26 -0700

    finished chall 9

--- Level Up Python/09 Simulate Dice/simulate_dice.py
+++ Level Up Python/09 Simulate Dice/simulate_dice.py
@@ -1,13 +1,31 @@
+# Importing the randint function from the random module
+from random import randint
+
+# Importing the Counter class from the collections module
+from collections import Counter
 
 
-def roll_dice(dice_list):
+# Defining a function named roll_dice which takes variable number of dice and an optional num_trials argument
+def roll_dice(*dice, num_trials=1_000_000):
+    # Initializing an empty Counter object to store outcome counts
+    counts = Counter()
 
-    min_num = len(dice_list)
-    max_num = sum(dice_list)
-    print(min_num, max_num)
+    # Simulating rolling the dice for num_trials times
+    for _ in range(num_trials):
+        # Generating a random outcome for each die and summing them up
+        counts[sum((randint(1, sides) for sides in dice))] += 1
 
-    print(dice_list)
+    # Printing header for the output
+    print('\nOUTCOME\tPROBABILITY')
+
+    # Iterating over possible outcomes
+    for outcome in range(len(dice), sum(dice) + 1):
+        # Calculating probability for each outcome and printing it
+        print(f'{outcome}\t{counts[outcome] * 100 / num_trials :0.2f}%')
 
 
-dice_list = [4, 6, 6]
-roll_dice(dice_list)
+# List representing the number of sides on each die
+lst = [4, 6, 6]
+
+# Calling the roll_dice function with the elements of lst as arguments
+roll_dice(*lst)
