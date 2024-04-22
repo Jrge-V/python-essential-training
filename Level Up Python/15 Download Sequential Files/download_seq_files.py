cea682281ef60373c6ed1cf57e09cc53becaf0d3
Jrge-V <jorgeverduzco91@gmail.com>
2024-04-21 18:45:37 -0700

    completed challenge 15

--- Level Up Python/15 Download Sequential Files/download_seq_files.py
+++ Level Up Python/15 Download Sequential Files/download_seq_files.py
@@ -4,6 +4,9 @@ import os
 
 
 def download_files(first_file_path, folder):
+    image_num = first_file_path[-7:-4]
+    image_num = int(image_num)
+    i = 1
 
     # check if folder exists, else create a new one
     if not os.path.exists(folder):
@@ -11,24 +14,32 @@ def download_files(first_file_path, folder):
 
     file_paths = first_file_path
 
-    for path in file_paths:
+    while True:
         try:
-            urllib.request.urlopen(path)
+            print(image_num)
+            urllib.request.urlopen(file_paths)
+            file_name = os.path.join(folder, f'image{i}.jpg')
+            i += 1
+
+            urllib.request.urlretrieve(file_paths, file_name)
+
+            image_num = int(image_num)
+            image_num += 1
+            if image_num < 10:
+                image_num = ('00' + str(image_num))
+            else:
+                image_num = ('0' + str(image_num))
+
+            file_paths = list(file_paths)
+            file_paths[-7:-4] = image_num
+            file_paths = ''.join(file_paths)
+
             print('found url')
+
         except HTTPError:
-            print('invalid url')
-
-
-
-
-    # file_name = os.path.join(folder, 'image1' + '.jpg')
-    # urllib.request.urlretrieve(file_path, file_name)
-
-
-
-    # TODO: figure out how to download multiples images
-    # TODO: Figure out how to sequentially add a +1 to the end of the url string for 50 files
-    # pass
+            print('HTTP ERROR: 404 invalid url')
+            print(file_paths)
+            break
 
 
 # TEST CODE
